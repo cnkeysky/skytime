e4cef0c32857c6c4e4dc45ef8b0eebf5d64b45b9
cnkeysky <32216693+cnkeysky@users.noreply.github.com>
2017-11-23 21:55:20 +0800

    Delete Stack.java

--- Stack.java
+++
@@ -1,9 +0,0 @@
-package 二叉树;
-
-public interface Stack<T> {
-
-    public abstract boolean isEmpty();
-    public abstract void push(T x);
-    public abstract T peek();
-    public abstract T pop();
-}
